5f8cdb0d9a24859a739b5f95fb7893dba7775f00
SSH4240 <pdxucxy@gmail.com>
2024-11-05 23:22:23 +0900

    .

---
+++ src/main/java/com/study/cruisin/CruisinApplication.java
@@ -0,0 +1,13 @@
+package com.study.cruisin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruisinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruisinApplication.class, args);
+	}
+
+}
