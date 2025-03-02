0cfa4d88ed9f660a4ffde73de7f9306c97c3ba02
SSH4240 <pdxucxy@gmail.com>
2025-03-02 21:01:22 +0900

    .

--- src/main/java/com/study/cruisin/entity/Member.java
+++ src/main/java/com/study/cruisin/entity/Member.java
@@ -10,7 +10,8 @@ import lombok.*;
 @ToString(of = {"id", "username", "age"})
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "member_id")
     private Long id;
     private String username;
@@ -18,21 +19,21 @@ public class Member {
 
     @ManyToOne
     @JoinColumn(name = "team_id")
-    // many to one have foreign key
     private Team team;
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null)
+            changeTeam(team);
+    }
+
     public void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
     }
-
-    public void changeUsername(String username) {
-        this.username = username;
-        this.age = 15;
-    }
-
 }
