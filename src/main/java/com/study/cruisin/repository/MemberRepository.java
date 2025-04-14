f3b631cc0eb68b9967a9393dca1294e1d20e0cc4
SSH4240 <pdxucxy@gmail.com>
2025-04-14 21:14:31 +0900

    .

--- src/main/java/com/study/cruisin/repository/MemberRepository.java
+++ src/main/java/com/study/cruisin/repository/MemberRepository.java
@@ -2,13 +2,11 @@ package com.study.cruisin.repository;
 
 import com.study.cruisin.dto.MemberDto;
 import com.study.cruisin.entity.Member;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
@@ -55,5 +53,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @EntityGraph(attributePaths = {"team"})
     @Query("select m from Member m")
     List<Member> findMemberEntityGraph();
+
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username);
+
 
 }
