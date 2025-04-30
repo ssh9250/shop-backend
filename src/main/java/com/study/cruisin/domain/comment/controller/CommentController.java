ee0c6657fbec6c3b97d0048f850423cc5ed522dc
SSH4240 <pdxucxy@gmail.com>
2025-04-30 21:30:41 +0900

    .

---
+++ src/main/java/com/study/cruisin/domain/comment/controller/CommentController.java
@@ -0,0 +1,14 @@
+package com.study.cruisin.domain.comment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comments")
+public class CommentController {
+    private final CommentController commentController;
+
+
+}
--- src/main/java/com/study/cruisin/domain/comment/repository/CommentRepositoryImpl.java
+++ src/main/java/com/study/cruisin/domain/comment/repository/CommentRepositoryImpl.java
@@ -1,12 +1,14 @@
 package com.study.cruisin.domain.comment.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.cruisin.domain.comment.entity.Comment;
+import com.study.cruisin.domain.comment.entity.QComment;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-import static com.study.cruisin.domain.comment.entity.QComment.comment;
+import static com.study.cruisin.domain.comment.entity.QComment.*;
 
 @RequiredArgsConstructor
 public class CommentRepositoryImpl implements CommentRepositoryCustom{
@@ -15,8 +17,15 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom{
     public List<Comment> findByPostId(Long postId) {
         return queryFactory
                 .selectFrom(comment)
-                .where(comment.post.id.eq(postId))
+                .where(getEq(postId))
                 .orderBy(comment.createdAt.desc())
                 .fetch();
+    }
+
+    private static BooleanExpression getEq(Long id) {
+        if (id == null) {
+            return null;
+        }
+        return comment.post.id.eq(id);
     }
 }
