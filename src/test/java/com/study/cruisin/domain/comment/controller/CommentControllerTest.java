ef0a026bd7d8f3803f9b6b6a4c1170faca367a32
SSH4240 <pdxucxy@gmail.com>
2025-05-29 22:41:26 +0900

    .

--- src/test/java/com/study/cruisin/domain/comment/controller/CommentControllerTest.java
+++ src/test/java/com/study/cruisin/domain/comment/controller/CommentControllerTest.java
@@ -52,15 +52,17 @@ class CommentControllerTest {
 
     @Test
     void 댓글_등록_가능() throws Exception {
+        //given
         CreateCommentRequestDto request = new CreateCommentRequestDto();
         request.setPostId(postId);
         request.setWriter("호랑이");
         request.setContent("<UNK> <UNK>");
 
-
+        //when
         mockMvc.perform(post("/api/comments")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
+                //then
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data").isNumber());
@@ -83,5 +85,20 @@ class CommentControllerTest {
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data").isArray())
                 .andExpect(jsonPath("$.data.length()").value(1));
+    }
+
+    @Test
+    void 존재하지_않는_댓글에는_댓글등록_불가() throws Exception {
+        CreateCommentRequestDto request = new CreateCommentRequestDto();
+        request.setPostId(999L);
+        request.setWriter("<UNK>");
+        request.setContent("<UNK> <UNK>");
+
+        mockMvc.perform(post("/api/comments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("게시글을 찾을 수 없습니다."));
     }
 }
