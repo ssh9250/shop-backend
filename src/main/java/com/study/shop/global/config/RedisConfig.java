fd3b50df04697d81028fdb4555abde29bbfe1540
ssh9250 <sh9250r@gmail.com>
2025-11-26 22:09:32 +0900

    feat: Redis 설정 추가 및 JWT 관련 클래스 구현, Swagger 보안 설정 개선

---
+++ src/main/java/com/study/shop/global/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.study.shop.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+}
---
+++ src/main/java/com/study/shop/global/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.study.shop.global.security.jwt;
+
+public class JwtAuthenticationFilter {
+}
