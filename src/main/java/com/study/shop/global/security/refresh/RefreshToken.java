eac9d83c91cb17fcb6d6ebd57d2e3380e31cc161
ssh9250 <sh9250r@gmail.com>
2025-11-29 21:42:26 +0900

    feat: refresh token 구현

--- src/main/java/com/study/shop/global/security/refresh/RefreshToken.java
+++ src/main/java/com/study/shop/global/security/refresh/RefreshToken.java
@@ -1,4 +1,17 @@
 package com.study.shop.global.security.refresh;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Schema
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshToken {
+    private String email;
+    private String token;
+    private long expiresAt;
+
 }
