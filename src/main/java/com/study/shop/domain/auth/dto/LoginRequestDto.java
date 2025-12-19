b700da871c0364297bb17dd728a9831757b1da9a
ssh9250 <sh9250r@gmail.com>
2025-12-19 21:24:27 +0900

    .

--- src/main/java/com/study/shop/domain/auth/dto/LoginRequestDto.java
+++ src/main/java/com/study/shop/domain/auth/dto/LoginRequestDto.java
@@ -1,19 +1,16 @@
 package com.study.shop.domain.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
     @NotBlank(message = "이메일은 필수입니다.")
     private String email;
     @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
-
-    public LoginRequestDto(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 }
